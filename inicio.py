5504ce5a58a8d2f2887adba6f93ad93c62098a2e
Patricia A. <74320237+mirsha08@users.noreply.github.com>
2024-04-25 19:41:02 +0200

    v0.2.1

--- inicio.py
+++ inicio.py
@@ -4,3 +4,7 @@ print("Hola Mundo")
 print("Segunda version")
 n_number = 45
 s_name= input("Dame tu nombre")
+
+#Vamos por la segunda version
+#recuperamos la variable nombre 
+print("Tu nombre es:", s_name)
